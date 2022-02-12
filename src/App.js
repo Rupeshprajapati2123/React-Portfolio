7219e884a0e9735f51c9c7682aa5f800d2196601
Rupeshprajapati2123 <77611959+Rupeshprajapati2123@users.noreply.github.com>
2022-02-12 23:39:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import './App.css';
+import "./App.css";
 import {BrowserRouter as Router} from 'react-router-dom';
  import Sidebar from './components/sidebar/Sidebar';
 import About from './components/about/About';
